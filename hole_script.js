cfbc9f8069384dc3438a346346d9765cb53bb537
TepesLily <lilok-ny@yandex.ru>
2022-09-25 16:55:51 +0300

    hole w stars

--- hole_script.js
+++ hole_script.js
@@ -1,9 +1,12 @@
 
 const hole = document.querySelector(".hole");
 const holeLink = document.querySelector(".hole__link");
+const starField = document.querySelector(".star_field");
 
 hole.addEventListener('click', () => {
     hole.classList.add("clicked");
     holeLink.classList.add("clicked");
+    starField.classList.add("clicked");
+
 })
 
